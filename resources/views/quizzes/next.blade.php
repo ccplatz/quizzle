436859ee95e3fb8efc8531e48bd5b28f3c7d402f
Carsten-Christian Platz <mail@ccplatz.de>
2023-10-28 14:04:50 +0200

    optimiize checking checkbox

--- resources/views/quizzes/next.blade.php
+++ resources/views/quizzes/next.blade.php
@@ -4,7 +4,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <form action="{{ route('quizzes.next', $quiz) }}" method="POST">
+                <form action="{{ route('quizzes.store-choices', $quiz) }}" method="POST">
+                    @csrf
                     <div class="card">
                         <div class="card-header">
                             {{ __('Question') }} {{ $quiz->questions->count() }} of {{ $quiz->number_of_questions }}
@@ -12,18 +13,20 @@
 
                         <div class="card-body">
                             <p>{{ $question->text }}</p>
-                            @csrf
                             @foreach ($question->answers as $answer)
-                                <div class="answer row border rounded my-2 py-1 pointer">
-                                    <div class="col-1 d-flex justify-content-center align-items-center">
-                                        {{ $answer->identifier }}</div>
-                                    <div class="col-10">{{ $answer->text }}</div>
-                                    <div class="col-1 d-flex justify-content-center align-items-center">
-                                        <input class="answer__check form-check-input mt-0"
-                                            name="answers[{{ $answer->id }}]" type="checkbox"
-                                            @checked(old('answers.' . $answer->id))>
+                                <label for="answerCheck-{{ $answer->id }}" class="d-block">
+                                    <div class="answer row border rounded my-2 py-1 pointer">
+                                        <div class="col-1 d-flex justify-content-center align-items-center">
+                                            {{ $answer->identifier }}</div>
+                                        <div class="col-10">{{ $answer->text }}</div>
+                                        <div class="col-1 d-flex justify-content-center align-items-center">
+                                            <input id="answerCheck-{{ $answer->id }}"
+                                                class="answer__check form-check-input mt-0"
+                                                name="choices[{{ $answer->id }}]" type="checkbox"
+                                                @checked(old('choices.' . $answer->id))>
+                                        </div>
                                     </div>
-                                </div>
+                                </label>
                             @endforeach
                         </div>
                     </div>
@@ -33,7 +36,3 @@
         </div>
     </div>
 @endsection
-
-@push('scripts')
-    @vite(['resources/js/quizzes/next.js'])
-@endpush
