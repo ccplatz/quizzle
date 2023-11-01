f9a5a9de23e137f4b7c007647d6bffe8268567f0
Carsten-Christian Platz <mail@ccplatz.de>
2023-11-01 16:59:01 +0100

    seeding quiz positions and choices

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,18 +19,33 @@ class DatabaseSeeder extends Seeder
     {
         User::factory()
             ->count(1)
-            ->has(Quiz::factory(2))
+            ->has(Quiz::factory(2)->state(['number_of_questions' => 10]))
             ->has(Question::factory(3)
                 ->has(Answer::factory(3)))
             ->create();
 
         User::factory()
-            ->has(Quiz::factory(2))
+            ->has(Quiz::factory(2)->state(['number_of_questions' => 10]))
             ->has(Question::factory(11)
                 ->has(Answer::factory(4)))
             ->create([
                 'name' => 'testuser',
                 'email' => 'test@test.de',
             ]);
+
+        foreach (Quiz::all() as $quiz) {
+            // link questions to quizzes
+            $questionsIdsInCurrentQuiz = $quiz->questions->map->only(['id'])->flatten()->toArray();
+            $questionsToAttach = Question::whereNotIn('id', $questionsIdsInCurrentQuiz)->get();
+            $questionsIdsToAttach = $questionsToAttach->random($quiz->number_of_questions)->map->only(['id'])->flatten()->toArray();
+            $quiz->questions()->attach($questionsIdsToAttach);
+            $quiz->refresh();
+
+            // set choices
+            foreach ($quiz->questions as $question) {
+                $question->quizPosition->answers()->attach($question->answers->random(rand(1, $question->answers->count()))->map->only(['id'])->flatten()->toArray());
+            }
+            ;
+        }
     }
 }
